21b8cb47ae08cd795b3efb8555ab980eabf380cf
krisaash <krisaash@gmail.com>
2016-04-11 08:12:50 +0200

    TEST-654 Add powershell script

---
+++ GenerateVersionDiffFile.ps1
@@ -0,0 +1,16 @@
+﻿function GetGitRepositoryDiff ($branch1, $branch2)
+{
+    $OFS = "`r`n"
+    $log = git log release/1..release/2
+    $html = [regex]::matches($log, "Test-\d*", 'MultiLine, IgnoreCase') | % { $_.Value } | sort -Unique
+    #$html = "<div><ul>" +$html +"</ul></div>"
+    $html = "" +$html
+
+	return $html
+}
+
+$content = GetGitRepositoryDiff 'release/1' 'release/2'
+
+echo $content
+
+New-Item ./diff.txt -type file -force -value $content 
